0c6f3bdb6188b70e10261c376137dcd76480d8d6
jctoala <julio.toalap@ug.edu.ec>
2025-03-29 22:28:29 -0500

    Add initial Terraform configuration and project structure; remove unused files

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "ruta_local" {
   type = string
-  description = "value of the local path"
+  description = "value of the local path to upload the files"
 }
 
 variable "region" {
   type = string
-  description = "value of the region"
+  description = "value of the region to create the resources"
 }
 
 variable "profile" {
   type = string
-  description = "value of the aws sso profile"
+  description = "value of the aws profile to use"
 }
 
 variable "group_tf_storage" {
